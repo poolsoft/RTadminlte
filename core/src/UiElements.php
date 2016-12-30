367dab406fc036436049906dc3cce039fd11363d
Roger Tiong RTdev <rogertiongdev@users.noreply.github.com>
2016-12-30 10:07:21 +0800

    Add files via upload

--- core/src/UiElements.php
+++ core/src/UiElements.php
@@ -15,6 +15,15 @@ namespace RTdev\RTadminlte;
  */
 class UiElements extends AdminLTE {
 
+    /**
+     * Generate alert box
+     *
+     * @param string $statusColor
+     * @param string $message
+     * @param string $title
+     * @param string $icon
+     * @return string
+     */
     public function alert($statusColor, $message = '', $title = '', $icon = '') {
 
         $nicon = (empty($icon) ? '' : sprintf('<i class="icon %s"></i>', (string) $icon));
@@ -27,6 +36,15 @@ class UiElements extends AdminLTE {
         return sprintf($html, self::getStatColor($statusColor), $nicon, (string) $title, (string) $message);
     }
 
+    /**
+     * Generate call out box
+     *
+     * @param string $statusColor
+     * @param string $message
+     * @param string $title
+     * @param string $icon
+     * @return string
+     */
     public function callout($statusColor, $message = '', $title = '', $icon = '') {
 
         $nicon = (empty($icon) ? '' : sprintf('<i class="icon %s"></i>', (string) $icon));
@@ -38,14 +56,43 @@ class UiElements extends AdminLTE {
         return sprintf($html, self::getStatColor($statusColor), $nicon, (string) $title, (string) $message);
     }
 
+    /**
+     * Generate label
+     *
+     * @param string $statusColor
+     * @param string $title
+     * @param string $pull "left" or "right"
+     * @return string
+     */
     public function label($statusColor, $title = '', $pull = '') {
 
         return sprintf('<span class="label label-lg label-%s %s">%s</span>', self::getStatColor($statusColor), self::getPullType($pull), (string) $title);
     }
 
+    /**
+     * Generate badge
+     *
+     * @param string $rawColor
+     * @param string $title
+     * @param string $pull "left" or "right"
+     * @return string
+     */
     public function badge($rawColor, $title = '', $pull = '') {
 
         return sprintf('<span class="badge badge-lg bg-%s %s">%s</span>', self::getRawColor($rawColor), self::getPullType($pull), (string) $title);
+    }
+
+    /**
+     * Generate custom label by using btn btn-xs
+     *
+     * @param string $rawColor
+     * @param string $title
+     * @param string $pull "left" or "right"
+     * @return string
+     */
+    public function labelXsBtn($rawColor, $title = '', $pull = '') {
+
+        return sprintf('<a class="btn btn-xs bg-%s %s" style="cursor:default;">%s</a>', self::getRawColor($rawColor), self::getPullType($pull), (string) $title);
     }
 
 }
